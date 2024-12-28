6e91ba42721f91936c1ff5e8d54fcd48a5921c6c
webOps-aman <amanhxe@gmail.com>
2024-12-29 01:10:51 +0530

    revision promise 4 part

---
+++ 3_promise/4_part.js
@@ -0,0 +1,25 @@
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("async four task is complete");
+        let error = false;
+        if(!error){
+            resolve({
+                username: "Surinder Kumar",
+                email: "surinder@gmail.com"
+            });
+        }else{
+            reject('Error Something went wrong');
+        }
+    },5000)
+});
+promiseFour
+.then(function(user){
+    console.log(user);
+    return user.username
+})
+.then(function(username){
+    console.log(username)
+})
+.catch(function(error){
+    console.log(error)
+})
